7c513d7e39438d21741ff6b56d239545281e7dbe
Aswin Mylraj <amylraj@endor.ai>
2024-11-08 11:41:30 +0530

    Updating POM and Creating More Commits To Test

--- src/main/java/com/endor/maven/App.java
+++ src/main/java/com/endor/maven/App.java
@@ -1,13 +1,19 @@
 package com.endor.maven;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
+        LOG.error("Generating Call Sites for Error");
         System.out.println( "Hello World!" );
     }
 }
